c48e9a93bcd2af5742a8911bbc4408d4d4fc8aa1
Buthead9250 <Phil9250@yandex.ru>
2020-12-06 16:59:50 +0700

    2.2. Рефакторинг - Площадь треугольника. [#403078]

--- src/main/java/ru/job4j/tracker/oop/Triangle.java
+++ src/main/java/ru/job4j/tracker/oop/Triangle.java
@@ -12,8 +12,7 @@ public class Triangle {
     }
 
     public double period(double a, double b, double c) {
-        double pp = (a + b + c) / 2;
-        return pp;
+        return ((a + b + c) / 2);
     }
 
     public boolean exist(double ab, double ac, double bc) {
