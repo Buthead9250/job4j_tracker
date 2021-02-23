d40f5325730bb71596480c8a160ccc70cf4f209c
Buthead9250 <Phil9250@yandex.ru>
2021-02-23 22:29:46 +0700

    3. Что такое JavaDoc [#402996]

--- src/main/java/ru/job4j/tracker/bank/BankService.java
+++ src/main/java/ru/job4j/tracker/bank/BankService.java
@@ -3,12 +3,26 @@ package ru.job4j.tracker.bank;
 import java.util.*;
 
 public class BankService {
+    /**
+     * Банковский сервис по осуществлению денежных переводов
+     */
     private Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Добавление нового пользователя в систему.
+     * Параметры метода: пользователи и список счетов.
+     * @param user добавляемый пользователь.
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<>());
     }
 
+    /**
+     * Поиск пользователя по паспорту и получение списка его счетов.
+     * Метод добавляет новый счет к существующему пользователю.
+     * @param passport паспорт пользователя.
+     * @param account список счетов.
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -19,6 +33,11 @@ public class BankService {
         }
     }
 
+    /**
+     * Поиск пользователя по паспорту.
+     * @param passport паспорт пользователя.
+     * @return возвращает найденного пользователя.
+     */
     public User findByPassport(String passport) {
         User rsl = null;
         for (User user : users.keySet()) {
@@ -30,6 +49,12 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Поиск пользователя по реквизитам.
+     * @param passport паспорт пользователя.
+     * @param requisite реквизиты счета пользователя.
+     * @return возвращает найденный счет по искомым реквизитам.
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         Account rslAccount = null;
@@ -45,6 +70,16 @@ public class BankService {
         return rslAccount;
     }
 
+    /**
+     * Метод осуществляет перевод денег с одного счета на другой.
+     * @param srcPassport паспорт отправителя денежных средств.
+     * @param srcRequisite реквизиты счета отправителя денежных средств.
+     * @param destPassport паспорт получателя денежных средств.
+     * @param destRequisite реквизиты счета получателя денежных средств.
+     * @param amount сумма средств на счете получателя после перечисления.
+     * @return если счет не найден или на счете srcAccount отсутствует
+     * необходимая для перевода сумма, то метод возвращает false
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
