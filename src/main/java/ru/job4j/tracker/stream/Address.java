e0b64166787ab877b3b22e1cae74046f27e9da9a
Buthead9250 <Phil9250@yandex.ru>
2021-03-27 12:51:28 +0700

    4. Преобразование List в Map. [#402845] - fix

--- src/main/java/ru/job4j/tracker/stream/Address.java
+++ src/main/java/ru/job4j/tracker/stream/Address.java
@@ -41,8 +41,12 @@ public class Address {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Address)) return false;
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Address)) {
+            return false;
+        }
         Address address = (Address) o;
         return home == address.home
                 && apartment == address.apartment
