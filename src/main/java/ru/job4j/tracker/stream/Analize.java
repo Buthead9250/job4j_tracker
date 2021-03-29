4a347d4812183da33f464c058e67dcbb08779fc0
Buthead9250 <Phil9250@yandex.ru>
2021-03-29 12:38:13 +0700

    9. fix

--- src/main/java/ru/job4j/tracker/stream/Analize.java
+++ src/main/java/ru/job4j/tracker/stream/Analize.java
@@ -15,8 +15,8 @@ public class Analize {
     }
 
     public static class User {
-        int id;
-        String name;
+        private int id;
+        private String name;
 
         public User(int id, String name) {
             this.id = id;
@@ -50,9 +50,9 @@ public class Analize {
     }
 
     public static class Info {
-        int added;
-        int changed;
-        int deleted;
+        private int added;
+        private int changed;
+        private int deleted;
 
         @Override
         public String toString() {
