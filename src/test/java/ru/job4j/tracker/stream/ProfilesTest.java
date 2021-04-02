ff4deeec4c41541f7007bfad96f9cdf0648763f4
Buthead9250 <Phil9250@yandex.ru>
2021-04-02 10:30:10 +0700

    2. Список адресов [#402843] - fix

--- src/test/java/ru/job4j/tracker/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/tracker/stream/ProfilesTest.java
@@ -3,7 +3,6 @@ package ru.job4j.tracker.stream;
 import org.junit.Test;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -12,36 +11,35 @@ public class ProfilesTest {
     @Test
     public void differentCollect() {
         Profiles func = new Profiles();
+        List<Profile> start = List.of(
+                new Profile(new Address("Барнаул", "Космическая", 13, 67)),
+                new Profile(new Address("Заринск", "Строителей", 31, 22)),
+                new Profile(new Address("Новосибирск", "Родники", 3, 129))
+        );
         List<Address> expect = List.of(
                 new Address("Барнаул", "Космическая", 13, 67),
                 new Address("Заринск", "Строителей", 31, 22),
                 new Address("Новосибирск", "Родники", 3, 129)
         );
-        List<Profile> input = expect.stream()
-                .map(Profile::new)
-                .collect(Collectors.toList());
-        List<Address> rsl = func.collect(input);
+        List<Address> rsl = func.collect(start);
         assertThat(rsl, is(expect));
     }
 
     @Test
     public void collectWithClone() {
         Profiles func = new Profiles();
-        List<Address> start = List.of(
-                new Address("Новосибирск", "Родники", 3, 129),
-                new Address("Барнаул", "Космическая", 13, 67),
-                new Address("Заринск", "Строителей", 31, 22),
-                new Address("Барнаул", "Космическая", 13, 67)
+        List<Profile> start = List.of(
+                new Profile(new Address("Новосибирск", "Родники", 3, 129)),
+                new Profile(new Address("Барнаул", "Космическая", 13, 67)),
+                new Profile(new Address("Заринск", "Строителей", 31, 22)),
+                new Profile(new Address("Барнаул", "Космическая", 13, 67))
         );
-        List<Profile> input = start.stream()
-                .map(Profile::new)
-                .collect(Collectors.toList());
         List<Address> expect = List.of(
                 new Address("Барнаул", "Космическая", 13, 67),
                 new Address("Заринск", "Строителей", 31, 22),
                 new Address("Новосибирск", "Родники", 3, 129)
         );
-        List<Address> rsl = func.collect(input);
+        List<Address> rsl = func.collect(start);
         assertThat(rsl, is(expect));
     }
 }
