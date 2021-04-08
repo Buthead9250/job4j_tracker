61b85ff947c491593bf91662b0812454d70aacba
Buthead9250 <Phil9250@yandex.ru>
2021-04-08 16:13:51 +0700

    2. Отсортировать департаменты [#402999]

--- src/main/java/ru/job4j/tracker/exam/DepDescComp.java
+++ src/main/java/ru/job4j/tracker/exam/DepDescComp.java
@@ -14,6 +14,6 @@ public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
         int result = right.split("/")[0].compareTo(left.split("/")[0]);
-        return result == 0 ? right.compareTo(left) : result;
+        return result == 0 ? left.compareTo(right) : result;
     }
 }
